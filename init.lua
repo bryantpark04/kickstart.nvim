d652dcf3b8fb11b4091750add8cdde627aba099f
Bryant Park <bryantp@palantir.com>
2026-01-14 17:59:58 -0500

    Enable formatters for Python and TypeScript

--- init.lua
+++ init.lua
@@ -794,10 +794,10 @@ require('lazy').setup({
         typescript = { 'dprint' },
         java = { 'palantir-java-format' },
         -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
+        python = { 'ruff' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
   },
