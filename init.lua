93691368dfd958fb78ef37afb5b99c5765d4c69c
Bryant Park <bryantp@palantir.com>
2026-01-22 17:29:26 -0500

    Treat .py.jinja2 files as Python files

--- init.lua
+++ init.lua
@@ -209,6 +209,13 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
+-- Treat .py.jinja2 files as Python
+vim.filetype.add {
+  pattern = {
+    ['.*%.py%.jinja2'] = 'python',
+  },
+}
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
