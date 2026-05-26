0ebba5a4283e6e4b7a690556dcf2acbaaf057216
Bryant Park <bryantp@palantir.com>
2026-05-26 11:05:09 -0400

    Nord -> Flexoki

--- init.lua
+++ init.lua
@@ -385,13 +385,12 @@ do
   -- change the command under that to load whatever the name of that colorscheme is.
   --
   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  vim.pack.add { gh 'gbprod/nord.nvim' }
-  require('nord').setup {}
+  vim.pack.add { gh 'kepano/flexoki-neovim' }
 
   -- Load the colorscheme here.
   -- Like many other themes, this one has different styles, and you could load
   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-  vim.cmd.colorscheme 'nord'
+  vim.cmd.colorscheme 'flexoki'
 
   -- Highlight todo, notes, etc in comments
   vim.pack.add { gh 'folke/todo-comments.nvim' }
